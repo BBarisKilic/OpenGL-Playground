b767163006f0e2796f489fafe4e25214144e3c15
bbkilic <bulentbariskilic@gmail.com>
2018-08-04 23:14:19 +0300

    Fog added.

---
+++ Fog/cmake-build-debug/CMakeFiles/Fog.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fog.dir/main.cpp.o"
+  "CMakeFiles/Fog.dir/display.cpp.o"
+  "Fog.pdb"
+  "Fog"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
